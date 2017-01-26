4765d6ae23ead41d6b6d8f5d24cf717c493894ac
lindl <lin_donglian@163.com>
2017-01-26 17:55:52 +0800

    update Get method

--- src/main/java/org/lindl/servlet/CoreServlet.java
+++ src/main/java/org/lindl/servlet/CoreServlet.java
@@ -29,9 +29,9 @@ public class CoreServlet extends HttpServlet {
 
         PrintWriter out = response.getWriter();
         // 通过检验signature对请求进行校验，若校验成功则原样返回echostr，表示接入成功，否则接入失败
-//        if (SignUtil.checkSignature(signature, timestamp, nonce)) {
+        if (SignUtil.checkSignature(signature, timestamp, nonce)) {
             out.print(echostr);
-//        }
+        }
         out.close();
         out = null;
     }
