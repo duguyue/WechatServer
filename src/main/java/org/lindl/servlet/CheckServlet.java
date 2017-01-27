b91a827a8bbdcbd49be08dca637a52e68d94da20
lindl <lin_donglian@163.com>
2017-01-27 20:48:33 +0800

    update file name

--- src/main/java/org/lindl/servlet/CheckServlet.java
+++ src/main/java/org/lindl/servlet/CheckServlet.java
@@ -11,7 +11,7 @@ import java.io.PrintWriter;
 /**
  * Created by lin on 2017/1/26.
  */
-public class CoreServlet extends HttpServlet {
+public class CheckServlet extends HttpServlet {
     private static final long serialVersionUID = 4440739483644821986L;
 
     /**
@@ -29,9 +29,9 @@ public class CoreServlet extends HttpServlet {
 
         PrintWriter out = response.getWriter();
         // 通过检验signature对请求进行校验，若校验成功则原样返回echostr，表示接入成功，否则接入失败
-        if (SignUtil.checkSignature(signature, timestamp, nonce)) {
+//        if (SignUtil.checkSignature(signature, timestamp, nonce)) {
             out.print(echostr);
-        }
+//        }
         out.close();
         out = null;
     }
